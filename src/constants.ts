2428c4aadaf3a73d8b67df0139057d697f49aac3
Oliver Joseph Ash <oliverjash@gmail.com>
2017-06-25 15:19:10 -0400

    Add OAuthAuthenticate endpoint to enum

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 export const TWITTER_API_BASE_URL = 'https://api.twitter.com';
 
 export enum ENDPOINTS {
+    OAuthAuthenticate = '/oauth/authenticate',
     OAuthRequestToken = '/oauth/request_token',
     OAuthAccessToken = '/oauth/access_token',
     StatusesHomeTimeline = '/1.1/statuses/home_timeline.json',
