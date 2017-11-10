a8471851a6abfa915ae72165756c6508a6a80110
Oliver Joseph Ash <oliverjash@gmail.com>
2017-11-10 19:56:54 +0000

    Replace custom `eitherTryCatchAsync` with `task.tryCatch`

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -2,25 +2,15 @@ import * as either from 'fp-ts/lib/Either';
 import * as task from 'fp-ts/lib/Task';
 import * as fetch from 'node-fetch';
 
-import { eitherTryCatchAsync } from './fp-ts';
-
 import Task = task.Task;
 import Either = either.Either;
-
-export type fetchPromiseEither = (
-    url: string,
-    init?: fetch.RequestInit,
-) => Promise<Either<Error, fetch.Response>>;
-export const fetchPromiseEither: fetchPromiseEither = (url, init) =>
-    eitherTryCatchAsync(
-        () => fetch.default(url, init),
-        // We assert that `fetch` will only ever throw `Error` instances
-        (error): Error => error as Error,
-    )();
 
 export type fetchTaskEither = (
     url: string,
     init?: fetch.RequestInit,
 ) => Task<Either<Error, fetch.Response>>;
 export const fetchTaskEither: fetchTaskEither = (url, init) =>
-    new Task(() => fetchPromiseEither(url, init));
+    task.tryCatch(() => fetch.default(url, init))(
+        // We assert that we'll only ever receive an `Error` instance from `fetch`
+        (error): Error => error as Error,
+    );
